24fea72c903370044baf938036c26eea0f77b5d7
Ahmed Hussein <wolktheline@yahoo.com>
2020-01-09 23:08:22 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,22 @@
-# Add your code here
+class Dog 
+@@all = []  
+
+  attr_accessor :name 
+  
+def initialize(name) 
+  @name = name 
+  @@all[]<< self 
+end
+
+def self.all
+  @@all = []
+end 
+def self.print_all
+  print_all.each do |a|
+    puts a 
+  end 
+end 
+  
+
+end 
+
